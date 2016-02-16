dfc5aef7ee8c81a273778f1ba9dc31a849de60b0
Zhihao Yang <yang_zhihao@live.com>
2016-02-16 19:30:03 +0100

    create view 6

--- js/app.js
+++ js/app.js
@@ -5,5 +5,6 @@ $(function() {
 	//And create the needed controllers and views
 	var guestNumberView = new GuestNumberView($("#guestNumberView"), model);
 	var selectDishView = new SelectDishView($("#selectDishView"), model);
+	var oneDishView = new OneDishView($("#oneDishView"), model);
 
 });
