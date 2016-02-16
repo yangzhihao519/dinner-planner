9ac2b2a189b6974ca4e9da7812edfa1c62a21550
Sakshyam <sakshyam1991@gmail.com>
2016-02-16 23:51:08 +0100

    rectified view6

--- js/view/prepareDishView.js
+++ js/view/prepareDishView.js
@@ -7,29 +7,35 @@ var PrepareDishView = function (container, model) {
 			
 
 	var dishId = ["1", "101"];
-	var prepareDishHtml = "";
+	var prepareDishHtml = "",
+		desp = "Description:"
 	
 	for(i=0; i< dishId.length; i++)
 	{
 		var dish = model.getDish(dishId[i]);
 		console.log(dish);
 		
-		prepareDishHtml += "<div class=\"col-md-12\">"+
-								"<div class=\"col-md-3 thumb\">"+
-									"<a class=\"thumbnail\">"+
-										"<img src='images/"+ dish.image + "'>"+
-									"</a>"+
-								"</div>"+
-								"<div class=\"col-md-3\">"+
-									+"<h3>"+dish.name +"</h3>"+
+		prepareDishHtml += "<div class=\"panel panel-default\">"+
+								"<div class=\"panel-body\">"+
 									"<div class=\"col-md-12\">"+
-										+"<span>"+dish.description + "</span>"+
+										"<div class=\"col-md-3 thumb\">"+
+											"<a class=\"thumbnail\">"+
+											"<img src='images/"+dish.image+"'>"+
+											"</a>"+
+										"</div>"+
+										"<div class=\"col-md-3\">"+
+											"<h3>"+dish.name+"</h3>"+
+											"<div class=\"col-md-12\">"+
+												"<span>"+dish.description+ "</span>"+
+											"</div>"+
+										"</div>"+
+										"<div class=\"col-md-6\">"+
+											"<h3>"+desp+"</h3>"+
+											"<span>"+dish.description + "</span>"+
+										"</div>"+
 									"</div>"+
 								"</div>"+
-								"<div class=\"col-md-6\">"+
-									+"<span>"+dish.description + "</span>"+
-								"</div>"+
-							"</div>";
+								"</div>";
 		
 	}
 	this.prepareDishes.html(prepareDishHtml);
